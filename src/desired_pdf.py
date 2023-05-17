de2fdee0adbe4563def37e999d0257fb099ec94e
Lars Rønhaug Pettersen <larsrpe@github.com>
2023-05-17 16:47:00 +0200

    hhs

--- src/desired_pdf.py
+++ src/desired_pdf.py
@@ -2,27 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from scipy import interpolate
-from typing import string, int, float
+
+import torch
 
 class Desired_PDF:
-    def __init__(self, image_path: string) -> None:
+    def __init__(self, image_path: str, L: float,device) -> None:
+        super().__init__()
         self.image = Image.open(image_path)
         self.image = self.image.convert('L')
         
         self.image_points = np.array(self.image)
         self.x_dim = self.image_points.shape[0]
         self.y_dim = self.image_points.shape[1]
+        self.dx = L/self.x_dim
+        self.dy = L/self.y_dim
 
-        self.f_interp = interpolate.RectBivariateSpline(np.arange(0, self.x_dim, 1), np.arange(0, self.y_dim, 1), self.image_points)
-        self.f_interp_int = self.f_interp.integral(0, self.x_dim, 0, self.y_dim)
-        self.f_nabla = self.f_interp.partial_derivative(1, 1) # 1'st order derivatives
+        self.f_interp = interpolate.RectBivariateSpline(np.linspace(0,L, self.x_dim), np.linspace(0,L, self.y_dim), self.image_points)
+        self.f_interp_int = self.f_interp.integral(0, L, 0, L)
+        self.f_nablax = self.f_interp.partial_derivative(1, 0) # 1'st order derivatives
+        self.f_nablay = self.f_interp.partial_derivative(0, 1)
 
-    def eval(self, x: int, y: int) -> float:
-        assert 0 <= x <= self.x_dim
-        assert 0 <= y <= self.y_dim
-        return self.f(x,y)/self.f_int
+        self.device = device
+
     
-    def grad(self, x: int, y: int) -> float:
-        assert 0 <= x <= self.x_dim
-        assert 0 <= y <= self.y_dim
-        return self.f_nabla(x, y)
+
+    def eval(self,r: torch.Tensor) -> torch.Tensor:
+        x = r[0].item()
+        y = r[1].item()
+        #assert 0 <= x <= self.x_dim
+        #assert 0 <= y <= self.y_dim
+        print(self.f_interp.ev(x,y))
+        return torch.Tensor(self.f_interp(x,y)/self.f_interp_int).reshape(-1).to(self.device)
+    
+    def grad(self,r: torch.Tensor) -> torch.Tensor:
+        x = r[0].item()
+        y = r[1].item()
+        #assert 0 <= x <= self.x_dim
+        #assert 0 <= y <= self.y_dim
+        return torch.Tensor([self.f_nablax(x, y)[0][0],self.f_nablay(x,y)[0][0]]).reshape(-1).to(self.device)
