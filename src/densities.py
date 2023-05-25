a563dacc3915273afaa10a98f8a9acd6d0be4a84
Mathias Skogli <mskogli@student.ethz.ch>
2023-05-25 12:53:50 +0200

    refactor

--- src/densities.py
+++ src/densities.py
@@ -3,9 +3,8 @@ import math
 
 from functools import cached_property
 
-
-class GausianPDF:
-    def __init__(self,sigma,t_0,T,mu_0,mu_T) -> None:
+class Gaussian_PDF:
+    def __init__(self, sigma: float, t_0: float, T, mu_0: torch.Tensor, mu_T: torch.Tensor) -> None:
         self.t_0 = t_0
         self.T = T
         self.mu_0 = mu_0
@@ -17,35 +16,29 @@ class GausianPDF:
     def var(self):
         return self.sigma**2
 
-
-    def _mu(self,t: float) -> torch.Tensor:
+    def _mu(self, t: float) -> torch.Tensor:
         if t < self.t_0:
             t = self.t_0
         if t > self.T:
             t = self.T
         return self.mu_0 + self._mu_dot(t)*(t-self.t_0)
     
-    def _mu_dot(self,t: float):
+    def _mu_dot(self, t: float):
         if t < self.t_0 or t > self.T:
             return 0
         return (self.mu_T-self.mu_0)/(self.T-self.t_0)
     
-    def eval(self,t: float,x: torch.Tensor) -> torch.Tensor:
+    def eval(self, t: float, x: torch.Tensor) -> torch.Tensor:
         mu = self._mu(t)
         return 1/(2*math.pi*self.var)*torch.exp(-1/(2*self.var)*torch.dot(x-mu,x-mu))
     
-    def grad(self,t: float, x: torch.Tensor) -> torch.Tensor:
+    def grad(self, t: float, x: torch.Tensor) -> torch.Tensor:
         mu = self._mu(t)
         return -(x-mu)/self.var*self.eval(t,x)
     
-    def dt(self,t: float,x: torch.Tensor) -> torch.Tensor:
+    def dt(self, t: float,x: torch.Tensor) -> torch.Tensor:
         return torch.dot(self.grad(t,x),self._mu_dot(t))
     
-    def get_g(self,t: float,x: float) -> torch.Tensor:
+    def get_g(self, t: float, x: float) -> torch.Tensor:
         g = self.eval(t,x)*self._mu_dot(t)
         return g
-    
-    
-
-    
-    
--- src/sim.py
+++ src/sim.py
@@ -1,58 +1,27 @@
 import torch
 import numpy as np
-from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation, FFMpegWriter
-from functools import partial
 
 from sys import path
+from scipy.integrate import solve_ivp
+from matplotlib.animation import FuncAnimation, FFMpegWriter
+from functools import partial
+from fields import VelocityField, LarsField
+
 path.append(".")
-from src.velocity_field import VelocityField
-from src.desired_pdf import GMM_PDF
-from src.lars_field import LarsField
-from src.timevarying_pdf import GausianPDF
 
-
-
-def dXdt(t,X: np.ndarray,VF: VelocityField,h) -> np.ndarray:
-    X = X.reshape(-1,2)
-    X = torch.from_numpy(X)
-    dXdt = np.empty_like(X)
-    for i,x in enumerate(X):
-        u = VF(x,X).numpy()
-        #print(np.linalg.norm(u))
-        dXdt[i,:] = grad_clip(u,0.1)
-    return dXdt.reshape(-1)
-
-def dXdt_lars(t,X: np.ndarray,LF: LarsField ,h) -> np.ndarray:
+def dXdt(t: float, X: np.ndarray, VF: VelocityField | LarsField, h: float) -> np.ndarray:
     print(t)
     X = X.reshape(-1,2)
     X = torch.from_numpy(X)
     dXdt = np.empty_like(X)
     for i,x in enumerate(X):
-        u = LF(t,x,X).numpy()
-        #print(np.linalg.norm(u))
-        dXdt[i,:] = u #grad_clip(u,0.1)
+        dXdt[i,:] = VF(t,x,X).numpy()
     return dXdt.reshape(-1)
 
-
-def grad_clip(grad,max):
-    norm = np.linalg.norm(grad)
-    if norm > max:
-        return grad/norm*max
-    return grad
-
-
-def sim(VF: VelocityField,X0,t_eval):
-    sol = solve_ivp(dXdt,y0=X0.reshape(-1),t_span=(0,t_eval[-1]),args=(VF,1),method="RK23",t_eval=t_eval)
-    
+def sim(VF: VelocityField | LarsField, X0: torch.Tensor, t_eval: np.ndarray) -> np.ndarray:
+    sol = solve_ivp(dXdt, y0=X0.reshape(-1), t_span=(0,t_eval[-1]), args=(VF,1), method="RK23", t_eval=t_eval)
     return sol.t,sol.y
-
-def simLars(LF: LarsField,X0,t_eval):
-    sol = solve_ivp(dXdt_lars,y0=X0.reshape(-1),t_span=(0,t_eval[-1]),args=(LF,1),method="RK23",t_eval=t_eval)
-    
-    return sol.t,sol.y
-    
 
 def viz(t,y,L):
     fig = plt.figure() 
@@ -82,37 +51,4 @@ def viz(t,y,L):
                      frames = len(t), interval = 20, blit = True)
   
     writer = FFMpegWriter(fps = 5)
-    anim.save('test.mp4',writer = writer)
-    
-
-if __name__ == "__main__":
-    device = "mps" if torch.backends.mps.is_available() else "cpu"
-    device = "cpu"
-
-    #means = torch.tensor([[0.1, 0.1], [0.4, 0.4], [0.1, 0.4], [0.4, 0.1], [0.25, 0.25]])
-    #covariances = torch.tensor([[[0.001, 0.0], [0.0, 0.001]],
-    #                            [[0.001, 0.0], [0.0, 0.001]],
-    #                            [[0.001, 0.0], [0.0, 0.001]],
-    #                            [[0.001, 0.0], [0.0, 0.001]],
-    #                            [[0.001, 0.0], [0.0, 0.001]]])
-    #weights = torch.tensor([0.1, 0.1, 0.1, 0.1, 0.6])
-    
-    N=100
-    L=15
-    sigma = 1
-    T = 100
-    mu_0 = torch.Tensor([3,3])
-    #X0 = torch.randn(N,2) + mu_0
-    X0 = L/2*torch.rand(N,2)
-    mu_T = torch.Tensor([13,13])
-    f_R = GausianPDF(sigma,50,50+0.5,mu_0,mu_T)
-    h = L/20
-    D = 5
-
-    #VF = VelocityField(f_R,h,D).to(device)
-    LF = LarsField(f_R,h,D)
-    t_eval = np.linspace(0,T,T*5+1,endpoint=True)
-    #t,y = sim(VF,X0,t_eval)
-    t,y = simLars(LF,X0,t_eval)
-    print("sim done")
-    viz(t,y,L)
+    anim.save('sims/test.mp4',writer = writer)
