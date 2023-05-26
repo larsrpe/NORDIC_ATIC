d285f7247595bb5e6bb4f4fa4958c2ad55ea18d6
Lars Rønhaug Pettersen <larsrpe@github.com>
2023-05-26 08:43:34 +0200

    fixed lena and some typehints

--- src/fields.py
+++ src/fields.py
@@ -1,39 +1,28 @@
 import torch
 import torch.nn as nn
-
 from torch.autograd import Variable
+from abc import ABC, abstractmethod
 from sys import path
-from kde import Gaussian_KDE
-from densities import Gaussian_PDF
 
 path.append(".")
+from src.kde import Gaussian_KDE
+from src.densities import TimevaryingPDF
 
-class VelocityField(nn.Module):
-    def __init__(self, f_R: Gaussian_PDF, h: float, D: float) -> None:
+
+class ControlField(nn.Module,ABC):
+    def __init__(self, f_d: TimevaryingPDF, h: float, D: float) -> None:
         super().__init__()
-        self.f_R = f_R
+        self.f_d = f_d
         self.KDE = Gaussian_KDE(h)
         self.D = D
 
-    def forward(self,r,X):
-        """
-        r: Tensor of shape
-        X: Tensor of shape N,d with all sampels 
+    @abstractmethod
+    def forward(self, t: float, r: torch.Tensor, X: torch.Tensor) -> torch.Tensor:
+        pass
 
-        returns the velocity field at r
-        """
-        r = Variable(r.clone(),requires_grad=True)
-        f_hat = self.KDE(r,X)
-        phi_grad = torch.autograd.grad(f_hat,r)[0] - self.f_R.grad(r)
-        return -self.D*phi_grad/f_hat.detach()
 
-class LarsField(nn.Module):
-    def __init__(self, f_R: Gaussian_PDF, h: float, D: float) -> None:
-        super().__init__()
-        self.f_R = f_R
-        self.KDE = Gaussian_KDE(h)
-        self.D = D
-
+class VelocityField(ControlField):
+    
     def forward(self, t: float, r: torch.Tensor, X: torch.Tensor) -> torch.Tensor:
         """
         r: Tensor of shape
@@ -41,10 +30,25 @@ class LarsField(nn.Module):
 
         returns the velocity field at r
         """
-        g = self.f_R.get_g(t,r)
         r = Variable(r.clone(),requires_grad=True)
         f_hat = self.KDE(r,X)
-        phi_grad = torch.autograd.grad(f_hat,r)[0] - self.f_R.grad(t,r)
+        phi_grad = torch.autograd.grad(f_hat,r)[0] - self.f_d.grad(t,r)
+        return -self.D*phi_grad/f_hat.detach()
+
+class LarsField(ControlField):
+    
+    def forward(self, t: float, r: torch.Tensor, X: torch.Tensor) -> torch.Tensor:
+        """
+        r: Tensor of shape
+        X: Tensor of shape N,d with all sampels 
+
+        returns the velocity field at r
+        """
+        
+        g,f_d_grad = self.f_d.get_g_and_grad(t,r)
+        r = Variable(r.clone(),requires_grad=True)
+        f_hat = self.KDE(r,X)
+        phi_grad = torch.autograd.grad(f_hat,r)[0] - f_d_grad
         V = -self.D*phi_grad
         
         return ((V + g)/f_hat).detach()
