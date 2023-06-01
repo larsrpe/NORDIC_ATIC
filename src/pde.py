60d320828fc6317e5e8a51d1a4a2462d18334df1
Lars Rønhaug Pettersen <larsrpe@github.com>
2023-06-01 10:04:35 +0200

    cont eq solver finished

--- src/pde.py
+++ src/pde.py
@@ -25,7 +25,7 @@ class ContEQSolver:
     
     def __init__(self,L: float, Rho: np.ndarray,Rho_dot: np.ndarray, Rho_grad: np.ndarray) -> None:
         self.L = L
-        self.N = Rho.shape[0]
+        self.N = Rho.shape[0]-1
         self.h = L/N
         self.V1 = None
         self.V2 = None
@@ -63,18 +63,17 @@ class ContEQSolver:
         Border = scipy.sparse.block_diag((B,B))
         #div = DIV@[g1;g2]
         DIV = scipy.sparse.hstack([dx,dy])
-        #rho
-        xx,yy = np.meshgrid(self.xs[1:-1],self.ys[1:-1])
-        rho = rho_f(xx,yy)
-        #rho*DIV
-       
+        
+        #Rho part of equation (rho*DIV(v))
+        #calculate rho on every gridpoint except the border, since central differnce is not defined here.
+        rho = self.Rho[1:self.N,1:self.N]
+        
+        # multiply with the DIV opertaor rho*DIV
         rhoDiag = scipy.sparse.csc_matrix(np.diag(rho.flatten()))
         rhoDIV = rhoDiag@DIV
         
-        #grad_rho * V
+        #Rho_grad part of equation (grad_x*v1 + grad_y*v2)
         
-
-
         # get border idx 
         border_grid = np.zeros((rows,cols))
         border_grid[:,0]=1
@@ -84,29 +83,33 @@ class ContEQSolver:
 
         not_border_mask = border_grid.flatten()==0
 
-        xx,yy = np.meshgrid(self.xs,self.ys)
-        grad_x = rho_grad_x(xx,yy)
+        #get gradient and make diagonal matrix 
+        grad_x = self.Rho_grad[:,:,0]
         grad_x_diag = np.diag(grad_x.flatten())
-        grad_y = rho_grad_y(xx,yy)
+        grad_y = self.Rho_grad[:,:,1]
         grad_y_diag = np.diag(grad_y.flatten())
 
+        
+
        
-        #remove zero rows
+        #remove rows that are connected to divergence at the border since its not defined due to central differences
         grad_x_diag=grad_x_diag[not_border_mask,:]
         grad_y_diag=grad_y_diag[not_border_mask,:]
-    
+
+        #concatenate 
         grad = scipy.sparse.hstack([scipy.sparse.csc_matrix(grad_x_diag),scipy.sparse.csc_matrix(grad_y_diag)])
         
-        # continuety
-        print(grad.shape,rhoDIV.shape,rho.shape)
+        #Define A and B in A@[v1.flatten();v2.flatten()]=B
+
+        #Central differnece approx of continuety equation -Rho_dot.flatten() = A_cont@[v1.flatten();v2_flatten()]
         A_cont = grad + rhoDIV
-        #solve Ax=B
+        #add border constraints
         A = scipy.sparse.vstack([A_cont,Border])
         #create B
         B = np.zeros(A.shape[0])
-        xx,yy = np.meshgrid(self.xs[1:-1],self.ys[1:-1])
-        Rho_dot = dot(xx,yy)
-        B[:Rho_dot.size] = -Rho_dot.flatten()
+        rho_dot = self.Rho_dot[1:self.N,1:self.N]
+        B[:rho_dot.size] = -rho_dot.flatten()
+        
         return A,B
 
 
@@ -114,21 +117,19 @@ class ContEQSolver:
         rows,cols = self.N+1,self.N+1
         A,B = self.get_spars_AB()
         v_vec = scipy.sparse.linalg.lsqr(A,B)[0]
-        self.G1 = v_vec[:rows*cols].reshape(rows,cols)
-        self.G2 = v_vec[rows*cols:].reshape(rows,cols)
+        self.V1 = v_vec[:rows*cols].reshape(rows,cols)
+        self.V2 = v_vec[rows*cols:].reshape(rows,cols)
 
     def check_sol(self):
         for i in range(1,self.N):
             for j in range(1,self.N):
                 x,y = self.xs[i],self.ys[j]
-                #g1 = self.G1.detach().numpy()[0,j,i]
-                #g2 = self.G2.detach().numpy()[0,j,i]
-                v1 = self.G1[j,i]
-                v2 = self.G2[j,i]
+                v1 = self.V1[j,i]
+                v2 = self.V2[j,i]
                 rho = rho_f(x,y)
                 rho_dot = dot(x,y)
-                v1x = (self.G1[j,i+1]-self.G1[j,i-1])/(2*self.h)
-                v2y = (self.G2[j+1,i]-self.G2[j-1,i])/(2*self.h)
+                v1x = (self.V1[j,i+1]-self.V1[j,i-1])/(2*self.h)
+                v2y = (self.V2[j+1,i]-self.V2[j-1,i])/(2*self.h)
                 rhox = rho_grad_x(x,y)
                 rhoy = rho_grad_y(x,y)
 
@@ -151,70 +152,77 @@ class ContEQSolver:
         for i in range(0,self.N+1,step):
             for j in range(0,self.N+1,step):
                 x,y = self.xs[i],self.ys[j]
-                #g1 = self.G1.detach().numpy()[0,j,i]
-                #g2 = self.G2.detach().numpy()[0,j,i]
-                g1 = self.G1[j,i]
-                g2 = self.G2[j,i]
-                norm = math.sqrt(g1**2 + g2**2)
+                v1 = self.V1[j,i]
+                v2 = self.V2[j,i]
                 xx[k] = x
                 yy[k] = y
-                rho = rho_f(x,y)
-                uu[k] = rho*g1 #/(norm) if norm > 5 else g1
-                vv[k] = rho*g2 #/(norm)*5 if norm >5 else g2
+                rho = self.Rho[j,i]
+                uu[k] = rho*v1
+                vv[k] = rho*v2
                 k+=1
         
-
         return xx[:k],yy[:k],uu[:k],vv[:k]
     
-def dot(x,y) -> float:
-    p1 = 0.9
-    p2 = 0.1
-    u1 = np.array([5,3.5])
-    u2 = np.array([5,5.5])
-    sigma = 1
-    var = sigma**2
-    return -1/(2*math.pi*var)*np.exp(-1/(2*var)*((x-u1[0])**2+(y-u1[1])**2))+ 1/(2*math.pi)*np.exp(-1/(2*var)*((x-u2[0])**2+(y-u2[1])**2))
 
-def rho_f(x,y) -> float:
-    p1 = 0.9
-    p2 = 0.1
-    u1 = np.array([5,3.5])
-    u2 = np.array([5,5.5])
-    sigma = 1
-    var = sigma**2
-    return p1/(2*math.pi*var)*np.exp(-1/(2*var)*((x-u1[0])**2+(y-u1[1])**2))+ p2/(2*math.pi)*np.exp(-1/(2*var)*((x-u2[0])**2+(y-u2[1])**2)) 
-
-def rho_grad_x(x,y):
-    p1 = 0.9
-    p2 = 0.1
-    u1 = np.array([5,3.5])
-    u2 = np.array([5,5.5])
-    sigma = 1
-    var = sigma**2
-    return -p1/(2*math.pi*var**2)*np.exp(-1/2*((x-u1[0])**2+(y-u1[1])**2))*(x-u1[0]) -p2/(2*math.pi*var**2)*np.exp(-1/2*((x-u2[0])**2+(y-u2[1])**2))*(x-u2[0])
-
-def rho_grad_y(x,y):
-    p1 = 0.9
-    p2 = 0.1
-    u1 = np.array([5,3.5])
-    u2 = np.array([5,5.5])
-    sigma = 1
-    var = sigma**2
-    return -p1/(2*math.pi*var**2)*np.exp(-1/2*((x-u1[0])**2+(y-u1[1])**2))*(y-u1[1]) -p2/(2*math.pi*var**2)*np.exp(-1/2*((x-u2[0])**2+(y-u2[1])**2))*(y-u2[1])
 
 
 if __name__ == "__main__":
-    #test_diff()
+
+    def dot(x,y) -> float:
+        p1 = 0.9
+        p2 = 0.1
+        u1 = np.array([5,3.5])
+        u2 = np.array([5,5.5])
+        sigma = 1
+        var = sigma**2
+        return -1/(2*math.pi*var)*np.exp(-1/(2*var)*((x-u1[0])**2+(y-u1[1])**2))+ 1/(2*math.pi)*np.exp(-1/(2*var)*((x-u2[0])**2+(y-u2[1])**2))
+
+    def rho_f(x,y) -> float:
+        p1 = 0.9
+        p2 = 0.1
+        u1 = np.array([5,3.5])
+        u2 = np.array([5,5.5])
+        sigma = 1
+        var = sigma**2
+        return p1/(2*math.pi*var)*np.exp(-1/(2*var)*((x-u1[0])**2+(y-u1[1])**2))+ p2/(2*math.pi)*np.exp(-1/(2*var)*((x-u2[0])**2+(y-u2[1])**2)) 
+
+    def rho_grad_x(x,y):
+        p1 = 0.9
+        p2 = 0.1
+        u1 = np.array([5,3.5])
+        u2 = np.array([5,5.5])
+        sigma = 1
+        var = sigma**2
+        return -p1/(2*math.pi*var**2)*np.exp(-1/2*((x-u1[0])**2+(y-u1[1])**2))*(x-u1[0]) -p2/(2*math.pi*var**2)*np.exp(-1/2*((x-u2[0])**2+(y-u2[1])**2))*(x-u2[0])
+
+    def rho_grad_y(x,y):
+        p1 = 0.9
+        p2 = 0.1
+        u1 = np.array([5,3.5])
+        u2 = np.array([5,5.5])
+        sigma = 1
+        var = sigma**2
+        return -p1/(2*math.pi*var**2)*np.exp(-1/2*((x-u1[0])**2+(y-u1[1])**2))*(y-u1[1]) -p2/(2*math.pi*var**2)*np.exp(-1/2*((x-u2[0])**2+(y-u2[1])**2))*(y-u2[1])
+
+
+        
     T = 10
     L = 10
-    N = 250
-    solver = ContEQSolver(L,N)
-    x= torch.tensor([30,10]).double()
-    solver.solve_rho(0.1)
-    #solver.solve_pytorch(t=0.1)
+    N = 100
+
+    xs = np.linspace(0,L,N+1)
+    ys = np.linspace(0,L,N+1)
+    xx,yy = np.meshgrid(xs,ys)
+    Rho = rho_f(xx,yy)
+    Rho_dot = dot(xx,yy)
+    Rho_grad = np.concatenate((rho_grad_x(xx,yy).reshape(N+1,N+1,1),rho_grad_y(xx,yy).reshape(N+1,N+1,1)),axis=2)
+    
+    solver = ContEQSolver(L,Rho,Rho_dot,Rho_grad)
+    solver.solve(0.1)
+
     xx,yy,uu,vv = solver.get_field(n=50)
     plt.quiver(xx,yy,uu,vv)
-    plt.savefig("./images/fd_pde.jpg")
+    plt.savefig("./images/fd_pde_test_rebase.jpg")
     solver.check_sol()
 
 
